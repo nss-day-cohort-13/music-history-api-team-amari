f09a707fa6b78240aff81c5d9ef165c54a644e39
Sulaiman.Allen <xalerons@gmail.com>
2016-09-07 15:24:19 -0500

    Updated urls

--- music_history/hanna/urls.py
+++ music_history/hanna/urls.py
@@ -1,2 +1,13 @@
 from rest_framework import routers
 from django.conf.urls import url, include
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'albums', views.AlbumList)
+router.register(r'artists', views.ArtistList)
+router.register(r'songs', views.SongList)
+
+urlpatterns = [
+	url(r'^', include(router.urls)),
+]
