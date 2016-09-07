f33883a82fb1ea734fd6d12e0599770487f8bafe
Sulaiman.Allen <xalerons@gmail.com>
2016-09-07 14:47:01 -0500

    Added model outlines

--- music_history/hanna/models.py
+++ music_history/hanna/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Artist(model.Model):
+    pass
+
+
+class Album(model.Model):
+    pass
+
+
+class Song(model.Model):
+    pass
---
+++ music_history/hanna/urls.py
@@ -0,0 +1,2 @@
+from rest_framework import routers
+from django.conf.urls import url, include
