a73ab3aa277bd520554fa8dd305bea759bd0e9f8
Megan Ford <meg.c.ford@gmail.com>
2016-09-08 11:14:54 -0500

    http get request loads all song titles.

--- russell/app/view.js
+++ russell/app/view.js
@@ -1,4 +1,18 @@
 angular.module("Russell")
-  .controller("View", function($scope) {
+  .controller("View", function($scope, $http, $timeout) {
     $scope.title="view page";
+
+    $scope.songs = [];
+    const errorHandle = (e) => console.log(e);
+
+    // Populate song data.
+    $http.get("http://localhost:8000")
+      .then((root) => {
+        return $http.get(`${root.data.songs}`);
+      }, errorHandle)
+      .then((songlist) => {
+        $scope.songs = songlist.data;
+        $timeout();
+      }, errorHandle);
+
   });
