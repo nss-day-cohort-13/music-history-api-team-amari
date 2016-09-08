9fa5ba0c05294ab4c5988d89d1c9605a26a8f9af
Megan Ford <meg.c.ford@gmail.com>
2016-09-08 09:52:37 -0500

    moved album many to many field to songs.

--- music_history/hannah/serializers.py
+++ music_history/hannah/serializers.py
@@ -5,28 +5,29 @@ from hannah.models import Album, Artist, Song
 # between the models, and the data, and provide a hyperlink to the
 # related resource instead of the primary key
 # http://www.django-rest-framework.org/tutorial/5-relationships-and-hyperlinked-apis/#hyperlinking-our-api
-class SongSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Song
-        fields = ('id', 'url', 'title')
-
 
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Artist
-        fields = ('id', 'url', 'title')
+        fields = ('id', 'url', 'name')
 
 
 class AlbumSerializer(serializers.HyperlinkedModelSerializer):
 
-    songs = SongSerializer(many=True, read_only=True)
     artists = ArtistSerializer(many=True, read_only=True)
 
     class Meta:
         model = Album
-        fields = ('id', 'url', 'title', 'songs', 'artists')
+        fields = ('id', 'url', 'title', 'artists')
+
+class SongSerializer(serializers.HyperlinkedModelSerializer):
+    
+    albums = AlbumSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Song
+        fields = ('id', 'url', 'title', 'albums')
 
 
 
