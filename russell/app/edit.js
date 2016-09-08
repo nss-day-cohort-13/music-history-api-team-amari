15751845c63cc9ba899601fad020ff7ce590a8d2
Sulaiman.Allen <xalerons@gmail.com>
2016-09-08 14:35:43 -0500

    Added creation of new songs

--- russell/app/edit.js
+++ russell/app/edit.js
@@ -1,13 +1,28 @@
 angular.module("Russell")
-  .controller("Edit", function($scope) {
-    $scope.title="edit page";
-    $scope.albumList = null
-    $scope.songList = null
-    $scope.artistList = null
+  .controller("Edit", function($scope, $timeout, RootFactory, NewItemFactory) {
+    $scope.title = "edit page";
 
-     $scope.items = ['one','two','three','four']
+	RootFactory.getSongs()
+	.then((songs) => {
+		$scope.songList = songs
+		console.log("$scope.songList = ", $scope.songList)
+	}).then(
+		RootFactory.getAlbums().then((albums) => {
+			$scope.albumList = albums
+			console.log("$scope.albumList = ", $scope.albumList)
+			}).then(
+				RootFactory.getArtists().then((artists) => {
+					$scope.artistList = artists
+					console.log("$scope.artistList = ", $scope.artistList)
+			})
+		)
+	).then($timeout())
 
-
-
+	$scope.submit = function(selectedSong, selectedAlbum, selectedArtist) {
+		console.log("In submit")
+		console.log("selectedAlbum = ", selectedAlbum)
+		result = `{"title": "${selectedSong}","albums": ["${selectedAlbum}"]}`
+		NewItemFactory.postItems(result)
+	}
 
   });
