6ccb2447f371924c520b5e847adc295344344e44
Megan Ford <meg.c.ford@gmail.com>
2016-09-08 10:08:50 -0500

    updated views, serializers to correctly relate many-to-many field.

--- music_history/hannah/serializers.py
+++ music_history/hannah/serializers.py
@@ -1,10 +1,6 @@
 from rest_framework import serializers
 from hannah.models import Album, Artist, Song
 
-# Using the HyperlinkedModelSerializer will examine the relationships
-# between the models, and the data, and provide a hyperlink to the
-# related resource instead of the primary key
-# http://www.django-rest-framework.org/tutorial/5-relationships-and-hyperlinked-apis/#hyperlinking-our-api
 
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
 
@@ -15,16 +11,12 @@ class ArtistSerializer(serializers.HyperlinkedModelSerializer):
 
 class AlbumSerializer(serializers.HyperlinkedModelSerializer):
 
-    artists = ArtistSerializer(many=True, read_only=True)
-
     class Meta:
         model = Album
         fields = ('id', 'url', 'title', 'artists')
 
 class SongSerializer(serializers.HyperlinkedModelSerializer):
     
-    albums = AlbumSerializer(many=True, read_only=True)
-
     class Meta:
         model = Song
         fields = ('id', 'url', 'title', 'albums')
