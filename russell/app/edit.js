aa5dbc500b7b3903b7cc61a6e7838d541c728b31
Sulaiman.Allen <xalerons@gmail.com>
2016-09-08 12:05:57 -0500

    Attempted migration and cancelled it

--- russell/app/edit.js
+++ russell/app/edit.js
@@ -1,4 +1,13 @@
 angular.module("Russell")
   .controller("Edit", function($scope) {
     $scope.title="edit page";
+    $scope.albumList = null
+    $scope.songList = null
+    $scope.artistList = null
+
+     $scope.items = ['one','two','three','four']
+
+
+
+
   });
